4b8246f4762827451b7293d0994d345e13fb63a9
orxanone <recebovorxan3@gmail.com>
2023-05-18 10:07:50 +0400

    Task

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+            int eded = 0;
+             for (int a = 0;a<10;a++)
+            {
+                int kub = a * a * a;
+                eded += kub;
+                Console.WriteLine(eded);
+
+
+
+
+            }
+        }
+    }
+}
